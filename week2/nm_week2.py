681843f55c1334b7c3cccf5276d81b017532ca99
nmccrea <nmgeneric@gmail.com>
2013-03-24 20:53:48 -0500

    added some real basic robot functionality

--- week2/nm_week2.py
+++ week2/nm_week2.py
@@ -4,9 +4,16 @@
 
 from math import *
 
-# Khepera3 Properties
-KHEPERA3_WHEEL_RADIUS = 0.021         # meters
-KHEPERA3_WHEEL_BASE_LENGTH = 0.0885   # meters
+
+
+# Khepera3 Properties (copied from Sim.I.Am)
+K3_WHEEL_RADIUS = 0.021         # meters
+K3_WHEEL_BASE_LENGTH = 0.0885   # meters
+K3_SPEED_FACTOR = 6.2953e-6     
+K3_TRANS_VEL_LIMIT = 0.3148     # m/s
+K3_ANG_VEL_LIMIT = 2.2763       # rad/s
+
+
 
 class DifferentialDriveDynamics:
   
@@ -15,8 +22,8 @@ class DifferentialDriveDynamics:
     self.wheel_base_length = wheel_base_length
     
   def uni_to_diff( self, v, w ):
-    # v = translational velocity (meters/sec)
-    # w = angular velocity (radians/sec)
+    # v = translational velocity (m/s)
+    # w = angular velocity (rad/s)
     
     R = self.wheel_radius
     L = self.wheel_base_length
@@ -27,8 +34,8 @@ class DifferentialDriveDynamics:
     return v_l, v_r
     
   def diff_to_uni( self, v_l, v_r ):
-    # v_l = left-wheel angular velocity (radians/sec)
-    # v_r = right-wheel angular velocity (radians/sec)
+    # v_l = left-wheel angular velocity (rad/s)
+    # v_r = right-wheel angular velocity (rad/s)
     
     R = self.wheel_radius
     L = self.wheel_base_length
@@ -39,18 +46,40 @@ class DifferentialDriveDynamics:
     return v, w
     
 
-#class Robot # Khepera3 robot, properties copied from Sim.I.Am
-#  
-#  def __init__:
-#    self.wheel_radius = KHEPERA3_WHEEL_RADIUS             # meters
-#    self.wheel_base_length = KHEPERA3_WHEEL_BASE_LENGTH   # meters
 
 
-#dynamics = DifferentialDriveDynamics( KHEPERA3_WHEEL_RADIUS, KHEPERA3_WHEEL_BASE_LENGTH )
-#v_l, v_r = dynamics.uni_to_diff( 3.0, (-pi / 2.0 ) )
-#print v_l * dynamics.wheel_radius
-#print v_r * dynamics.wheel_radius
-#print "\n\n"
-#v, w = dynamics.diff_to_uni( 3 / KHEPERA3_WHEEL_RADIUS, 6 / KHEPERA3_WHEEL_RADIUS )
-#print str(v) + " m/s"
-#print str(w/pi) + "pi rad/sec"
+class Robot: # Khepera3 robot 
+  
+  def __init__( self ):
+    # wheel arrangement:
+    self.wheel_radius = K3_WHEEL_RADIUS             # meters
+    self.wheel_base_length = K3_WHEEL_BASE_LENGTH   # meters
+    
+    # wheel speed factor
+    self.speed_factor = K3_SPEED_FACTOR
+    
+    # wheel speeds (rad/sec)
+    self.left_wheel_rotation = 0.0
+    self.right_wheel_rotation = 0.0
+    
+    # dynamics
+    self.dynamics = DifferentialDriveDynamics( self.wheel_radius, self.wheel_base_length )
+    
+  def update_state( self ):
+    False
+  
+  def set_wheel_rotations( self, v_l, v_r ):
+    # limit the speeds:
+    v, w = self.dynamics.diff_to_uni( v_l, v_r )
+    v = max( min( v, K3_TRANS_VEL_LIMIT ), -K3_TRANS_VEL_LIMIT )
+    w = max( min( w, K3_ANG_VEL_LIMIT ), -K3_ANG_VEL_LIMIT )
+    v_l, v_r = self.dynamics.uni_to_diff( v, w )
+    
+    # set rotations
+    self.left_wheel_rotation = v_l
+    self.right_wheel_rotation = v_r
+    
+  
+  def tick( self ):
+    False
+  
