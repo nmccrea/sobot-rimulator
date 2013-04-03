1b1a05f96d99a375550f83c53cc91390586b7fd7
nmccrea <nmgeneric@gmail.com>
2013-04-03 16:41:31 -0500

    smartly drawing gridlines

--- week2/Simulator/views/world_view.py
+++ week2/Simulator/views/world_view.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 # -*- Encoding: utf-8 -*
+
 # Python implementation of the Week 2 exercise.
 
 import Euv.EuvGtk as Euv
@@ -8,13 +9,19 @@ import Euv.Shapes as Shapes
 import Euv.Color as Color
 from robot_view import *
 
+VIEW_PORT_PIX_H = 800
+VIEW_PORT_PIX_W = 800
+CONTROLS_PIX_H = 50
+
+WORLD_WIDTH = 10 # meters
+
 class WorldView:
 
   def __init__( self ):
     # create viewer
-    self.viewer = Euv.Viewer( size = (300, 300),
+    self.viewer = Euv.Viewer( size = (VIEW_PORT_PIX_W, VIEW_PORT_PIX_H+CONTROLS_PIX_H),
                           view_port_center = (0, 0),
-                          view_port_width = 3,
+                          view_port_width = WORLD_WIDTH,
                           flip_y = True )
 
     # initialize the current frame object
@@ -39,20 +46,37 @@ class WorldView:
     self.viewer.add_frame( self.current_frame )   # push the current frame
     self.current_frame = Frame.Frame()            # prepare the next frame
   
+  def wait( self ):
+    self.viewer.wait()
+
   def _draw_grid_to_frame( self ):
+    # NOTE: THIS FORMULA ASSUMES THE FOLLOWING:
+    # - Window size never changes
+    # - Window is always centered at (0, 0)
+
+    w = self._meters_per_pixel() * VIEW_PORT_PIX_W
+    h = self._meters_per_pixel() * VIEW_PORT_PIX_H
+    half_w = w / 2.0
+    half_h = h / 2.0
+
+    w_start = int( half_w )
+    w_end = int( w )
+    h_start = int( half_h )
+    h_end = int( h )
+    
     # draw a gridline at every meter
     lines = []
-    for x in range( -5, 6 ):
-      for y in range( -5, 6 ):
-        h_gridline = [ [ -5, y ], [ 5, y ] ]
-        v_gridline = [ [ x, -5 ], [ x, 5 ] ]
+    for x in range( -w_start, w_end ):
+      for y in range( -h_start, h_end ):
+        h_gridline = [ [ -half_w, y ], [ half_w, y ] ]
+        v_gridline = [ [ x, -half_h ], [ x, half_h ] ]
         lines.append( h_gridline )
         lines.append( v_gridline )
 
     self.current_frame.add_lines( lines,
-                     linewidth = 0.001,
+                     linewidth = self._meters_per_pixel(),
                      color = "black",
-                     alpha = 1.0 )
+                     alpha = 0.2 )
 
-  def wait( self ):
-    self.viewer.wait()
+  def _meters_per_pixel( self ):
+    return float( WORLD_WIDTH ) / float( VIEW_PORT_PIX_W )
