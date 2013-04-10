43973c89bcf213eb33e485723a5ca9030a1e30ea
nmccrea <nmgeneric@gmail.com>
2013-04-09 20:02:09 -0500

    added several linalg methods for rotatin and translating vectors and sets of vectors

--- week2/Simulator/utils/linalg2_util.py
+++ week2/Simulator/utils/linalg2_util.py
@@ -3,15 +3,6 @@
 
 from math import *
 
-# rotate the given vector by theta radians
-def rotate_vector( a, theta ):
-  sin_theta = sin( theta )
-  cos_theta = cos( theta )
-  
-  a0 = a[0]*cos_theta - a[1]*sin_theta
-  a1 = a[0]*sin_theta + a[1]*cos_theta
-
-  return [ a0, a1 ]
 
 # get the sum of two vectors
 def add( a, b ):
@@ -42,3 +33,38 @@ def lunormal( a ):
 def proj( a, b ):
   scale = float( dot( a, b ) ) / ( b[0]**2 + b[1]**2 )
   return( [ scale*b[0], scale*b[1] ] )
+
+# get the result of rotating a vector by theta radians
+def rotate_vector( a, theta ):
+  sin_theta = sin( theta )
+  cos_theta = cos( theta )
+  
+  a0 = a[0]*cos_theta - a[1]*sin_theta
+  a1 = a[0]*sin_theta + a[1]*cos_theta
+
+  return [ a0, a1 ]
+
+# get the result of rotating a set of vectors by theta radians
+def rotate_vectors( vects, theta ):
+  sin_theta = sin( theta )
+  cos_theta = cos( theta )
+  
+  rotvects = []
+  for a in vects:
+    a0 = a[0]*cos_theta - a[1]*sin_theta
+    a1 = a[0]*sin_theta + a[1]*cos_theta
+    rotvects.append( [ a0, a1 ] )
+
+  return rotvects
+
+# get the result of rotating and translating a vector
+def rotate_and_translate_vector( a, tvect, theta ):
+  return add( rotate_vector( a, theta ), tvect )
+
+# get the rsult of rotating and translating a set of vectors
+def rotate_and_translate_vectors( vects, tvect, theta ):
+  rtvects = []
+  for a in rotate_vectors( vects, theta ):
+    rtvects.append( add( a, tvect ) )
+
+  return rtvects
