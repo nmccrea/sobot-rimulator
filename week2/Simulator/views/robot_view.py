54efa6afa2e95f0061c48345993e36a7bef13c5f
nmccrea <nmgeneric@gmail.com>
2013-05-14 15:19:27 -0500

    some cosmetic stuff

--- week2/Simulator/views/robot_view.py
+++ week2/Simulator/views/robot_view.py
@@ -6,13 +6,13 @@ import utils.linalg2_util as linalg
 from proximity_sensor_view import *
 
 # Khepera3 Dimensions (copied from Sim.I.Am by J.P. de la Croix)
-K3_TOP_PLATE = [  [ -0.031, 0.043 ],
-                  [ -0.031, -0.043 ],
-                  [ 0.033, -0.043 ],
-                  [ 0.052, -0.021 ],
-                  [ 0.057, 0.0 ],
-                  [ 0.052, 0.021 ],
-                  [ 0.033, 0.043 ] ]
+K3_TOP_PLATE = [[ -0.031,  0.043 ],
+                [ -0.031, -0.043 ],
+                [  0.033, -0.043 ],
+                [  0.052, -0.021 ],
+                [  0.057,  0.000 ],
+                [  0.052,  0.021 ],
+                [  0.033,  0.043 ]]
 
 class RobotView:
   
