a2c5e34fec8095b681f039ddc1c28a021cffaa4b
nmccrea <nmgeneric@gmail.com>
2013-05-20 16:40:30 -0500

    slight performance improvement in angle normalization

--- utils/math_util.py
+++ utils/math_util.py
@@ -5,15 +5,8 @@ from math import *
 
 # map the given angle to the equivalent angle in [ -pi, pi ]
 def normalize_angle( theta ):
-  if theta > pi:
-    # subtracts a positive number of circles
-    theta -= 2 * pi * int( ( theta + pi ) / ( 2 * pi ) )
-  elif theta < -pi:
-    # subtracts a negative number of circles
-    theta -= 2 * pi * int( ( theta - pi ) / ( 2 * pi ) )
-
-  return theta
-  
+  return atan2( sin( theta ), cos( theta ) )
+ 
 def frange( x, y, jump = 1.0 ):
   r = []
   while x < y:
