92dfb2782dc42fadf72af537288b1838897cb7c8
nmccrea <nmgeneric@gmail.com>
2013-05-28 14:50:18 -0500

    fixed incorrect implementation of wheel encoder behavior

--- models/wheel_encoder.py
+++ models/wheel_encoder.py
@@ -8,12 +8,14 @@ class WheelEncoder( Sensor ):
 
   def __init__( self, ticks_per_rev ):
     self.ticks_per_rev = ticks_per_rev
+    self.real_revs = 0.0
     self.tick_count = 0
 
   # update the tick count for this wheel encoder
   # takes a float representing the number of forward revolutions made
   def step_revolutions( self, revolutions ):
-    self.tick_count += int( revolutions * self.ticks_per_rev )
+    self.real_revs += revolutions
+    self.tick_count = int( self.real_revs * self.ticks_per_rev )
 
   def read( self ):
     return self.tick_count
