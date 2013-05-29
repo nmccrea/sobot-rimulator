c445ff10e636a7568319d554ce9390b1c118c98f
nmccrea <nmgeneric@gmail.com>
2013-05-28 20:10:44 -0500

    initial framework for the avoid-obstacles controller

---
+++ models/controllers/avoid_obstacle_controller.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- Encoding: utf-8 -*
+
+from math import *
+
+from utils import linalg2_util as linalg
+from utils import math_util
+
+class AvoidObstacleController:
+
+  def __init__( self, supervisor ):
+    # bind the supervisor
+    self.supervisor = supervisor
+
+    # sensor gains (weights)
+    self.sensor_gains = [   1.0-( (0.9*abs(p.theta)) / pi )   for p in supervisor.sensor_placements() ]
+
+    # control gains
+    self.kP = 5.0
+    self.kI = 0.5
+    self.kD = 0.1
+    
+    # stored values - for computing next results
+    self.prev_time = 0.0
+    self.prev_eP = 0.0
+    self.prev_eI = 0.0
+
+  def execute( self ):
+    False
