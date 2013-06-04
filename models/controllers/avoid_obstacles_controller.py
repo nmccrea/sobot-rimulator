6d770ed9dec6d93b0298026d62f7a6735fa15181
nmccrea <nmgeneric@gmail.com>
2013-06-04 17:41:50 -0500

    partway through building the avoid-obstacles controller logic

--- models/controllers/avoid_obstacles_controller.py
+++ models/controllers/avoid_obstacles_controller.py
@@ -12,6 +12,9 @@ class AvoidObstaclesController:
     # bind the supervisor
     self.supervisor = supervisor
 
+    # sensor placements
+    self.proximity_sensor_placements = supervisor.proximity_sensor_placements()
+
     # sensor gains (weights)
     self.sensor_gains = [   1.0-( (0.9*abs(p.theta)) / pi )   for p in supervisor.proximity_sensor_placements() ]
 
@@ -26,4 +29,22 @@ class AvoidObstaclesController:
     self.prev_eI = 0.0
 
   def execute( self ):
-    False
+    # determine the global pose of each sensor
+    robot_pose = self.supervisor.estimated_pose()
+    global_sensor_poses = []
+    for p in self.proximity_sensor_placements:
+      global_sensor_poses.append( p.transform_to( robot_pose ).vunpack() )
+
+    # calculate the position of detected obstacles in the robot's reference frame
+    sensor_distances = self.supervisor.proximity_sensor_real_distances()
+    target_vectors = []
+    for i in range( len( sensor_distances ) ):
+      if sensor_distances[i] != None:
+        # calculate the position of the obstacle in the robot's reference frame
+        sensor_pos, sensor_theta = global_sensor_poses[i]
+        vector = [ 1.0, 0.0 ] # the neutral unit vector
+        vector = linalg.scale( vector, sensor_distances[i] )
+        vector = linalg.rotate_and_translate_vector( vector, sensor_theta, sensor_pos )
+        target_vectors.append( vector )
+      else:
+        target_vectors.append( [ 0.0, 0.0 ] )
