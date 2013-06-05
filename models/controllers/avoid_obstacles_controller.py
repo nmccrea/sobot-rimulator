fd2f443b4faab42dfab91d3bf84c2e03092b7410
nmccrea <nmgeneric@gmail.com>
2013-06-05 17:21:32 -0500

    avoid-obstacles controller complete logic (current gains and settings do give good robot behavior)

--- models/controllers/avoid_obstacles_controller.py
+++ models/controllers/avoid_obstacles_controller.py
@@ -19,7 +19,7 @@ class AvoidObstaclesController:
     self.sensor_gains = [   1.0-( (0.9*abs(p.theta)) / pi )   for p in supervisor.proximity_sensor_placements() ]
 
     # control gains
-    self.kP = 5.0
+    self.kP = 10.0
     self.kI = 0.5
     self.kD = 0.1
     
@@ -28,23 +28,59 @@ class AvoidObstaclesController:
     self.prev_eP = 0.0
     self.prev_eI = 0.0
 
-  def execute( self ):
-    # determine the global pose of each sensor
-    robot_pose = self.supervisor.estimated_pose()
-    global_sensor_poses = []
-    for p in self.proximity_sensor_placements:
-      global_sensor_poses.append( p.transform_to( robot_pose ).vunpack() )
+    # additional calculated values 
+    self.obstacle_vectors_rel = [ [ 0.0, 0.0 ] ] * len( self.proximity_sensor_placements )
+    self.obstacle_vectors_abs = [ [ 0.0, 0.0 ] ] * len( self.proximity_sensor_placements )
+    self.heading_vector = [ 0.0, 0.0 ]
 
-    # calculate the position of detected obstacles in the robot's reference frame
+  def execute( self ):
+    # get the distances indicated by the robot's sensor readings
     sensor_distances = self.supervisor.proximity_sensor_real_distances()
-    target_vectors = []
+
+    # calculate the position of detected obstacles and find an avoidance vector
+    heading_vector = [ 0.0, 0.0 ]        # initialize the heading vector
+    robot_pos, robot_theta = self.supervisor.estimated_pose().vunpack()
     for i in range( len( sensor_distances ) ):
-      if sensor_distances[i] != None:
-        # calculate the position of the obstacle in the robot's reference frame
-        sensor_pos, sensor_theta = global_sensor_poses[i]
-        vector = [ 1.0, 0.0 ] # the neutral unit vector
-        vector = linalg.scale( vector, sensor_distances[i] )
-        vector = linalg.rotate_and_translate_vector( vector, sensor_theta, sensor_pos )
-        target_vectors.append( vector )
-      else:
-        target_vectors.append( [ 0.0, 0.0 ] )
+      # calculate the position of the obstacle
+      sensor_pos, sensor_theta = self.proximity_sensor_placements[i].vunpack()
+      vector = [ sensor_distances[i], 0.0 ]
+      vector = linalg.rotate_and_translate_vector( vector, sensor_theta, sensor_pos )
+      self.obstacle_vectors_rel[i] = vector   # store the obstacle vectors in the robot's reference frame
+      vector = linalg.rotate_and_translate_vector( vector, robot_theta, robot_pos )
+      self.obstacle_vectors_abs[i] = vector   # store the obstacle vectors in the estimated world reference frame
+       
+      # accumluate the heading vector within the robot's reference frame
+      heading_vector = linalg.sub(  heading_vector,
+                                    linalg.scale( self.obstacle_vectors_rel[i], self.sensor_gains[i] ) )
+
+    # convert the final heading vector to the estimated world reference frame
+    heading_vector = linalg.rotate_and_translate_vector( heading_vector, robot_theta, robot_pos )
+    self.heading_vector = heading_vector
+    
+    # calculate the desired heading
+    theta_d = atan2( heading_vector[1], heading_vector[0] )
+
+    # calculate the time that has passed since the last control iteration
+    current_time = self.supervisor.time()
+    dt = current_time - self.prev_time
+
+    # calculate the error terms
+    eP = math_util.normalize_angle( theta_d - robot_theta )
+    eI = self.prev_eI + eP*dt
+    eD = ( eP - self.prev_eP ) / dt
+
+    # calculate angular velocity
+    omega = self.kP * eP + self.kI * eI + self.kD * eD
+    
+    # calculate translational velocity
+    # velocity is v_max when omega is 0,
+    # drops rapidly to zero as |omega| rises
+    v_max = 2.0 # TODO: move this
+    v = v_max / ( abs( omega ) + 1 )**2
+
+    # store values for next control iteration
+    self.prev_time = current_time
+    self.prev_eP = eP
+    self.prev_eI = eI
+
+    self.supervisor.set_outputs( v, omega )
