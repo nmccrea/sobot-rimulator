3842c5676489ae9b4fa4f706e38bfe05f82ee602
nmccrea <nmgeneric@gmail.com>
2013-06-19 11:41:53 -0500

    method for drawing rich traverse path that indicates robot speeds along the path - not used but available

--- views/robot_view.py
+++ views/robot_view.py
@@ -62,3 +62,30 @@ class RobotView:
     frame.add_lines(  [ self.traverse_path ],
                       color = "black",
                       linewidth = 0.01 )
+
+  # draws the traverse path as dots weighted according to robot speed
+  def _draw_rich_traverse_path_to_frame( self, frame ):
+    position = self.robot.pose.vposition()
+    self.traverse_path.append( position )
+
+    # when robot is moving fast, draw small, opaque dots
+    # when robot is moving slow, draw large, transparent dots
+    d_min,  d_max = 0.0, 0.01574  # possible distances between dots
+    r_min,  r_max = 0.007, 0.02   # dot radius
+    a_min,  a_max = 0.3, 0.55     # dot alpha value
+    m_r = ( r_max - r_min ) / ( d_min - d_max )
+    b_r = r_max - m_r*d_min
+    m_a = ( a_max - a_min ) / ( r_min - r_max )
+    b_a = a_max - m_a*r_min
+    
+    prev_posn = self.traverse_path[0]
+    for posn in self.traverse_path[1::1]:
+      d = linalg.distance( posn, prev_posn )
+      r = ( m_r*d ) + b_r
+      a = ( m_a*r ) + b_a
+      frame.add_circle( pos = posn,
+                        radius = r,
+                        color = "black",
+                        alpha = a)
+
+      prev_posn = posn
