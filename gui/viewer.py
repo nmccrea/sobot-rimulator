24fa07120c89807d22c954a75335b3cacbb6d10d
maddypie <tsadarsh070707@gmail.com>
2020-08-11 23:11:54 +0530

    Removed hard coded screen width and height, replaced with tkinter's methods

--- gui/viewer.py
+++ gui/viewer.py
@@ -24,12 +24,20 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk as gtk
 
+import tkinter as tk
+
 from gui.frame import Frame
 from gui.painter import Painter
 
-DEFAULT_VIEW_PIX_W = 600    # pixels
-DEFAULT_VIEW_PIX_H = 600    # pixels
-DEFAULT_ZOOM = 100          # pixels per meter
+
+root = tk.Tk()
+SCREEN_W = root.winfo_screenwidth()
+SCREEN_H = root.winfo_screenheight()
+root.destroy()
+
+DEFAULT_VIEW_PIX_W = 0.5*SCREEN_W    # pixels
+DEFAULT_VIEW_PIX_H = 0.7*SCREEN_H    # pixels
+DEFAULT_ZOOM = 100                   # pixels per meter
 
 # user response codes for file chooser dialog buttons
 LS_DIALOG_RESPONSE_CANCEL = 1
